43cbdde9b2e426b26134447e5ec87a45eee85d82
Martijn Visser <mgvisser@gmail.com>
2018-09-06 14:48:19 +0200

    final deprecation warnings

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using LibLAS
 using Test
 
-info("Using LibLAS version $(LibLAS.liblas_version())")
+@info "Using LibLAS version $(LibLAS.liblas_version())"
 
 # testfile downloaded from http://www.liblas.org/samples/
 filename = "libLAS_1.2.las"
@@ -17,25 +17,31 @@ bounds = [LibLAS.min_x(header), LibLAS.min_y(header), LibLAS.min_z(header),
 @test bounds == [1440000.0, 375000.03, 832.1800000000001,
                  1444999.96, 379999.99, 972.6700000000001]
 
-x_sum = 0.0
-y_sum = 0.0
-z_sum = 0.0
+function mean_coordinates(header, reader)
+    n = LibLAS.pointrecordscount(header)
+    x_sum = 0.0
+    y_sum = 0.0
+    z_sum = 0.0
 
-# looping over all points
-@time for i = 1:n
-    p = LibLAS.nextpoint(reader)
-    x = LibLAS.xcoord(p)
-    y = LibLAS.ycoord(p)
-    z = LibLAS.zcoord(p)
+    # looping over all points
+    for i = 1:n
+        p = LibLAS.nextpoint(reader)
+        x = LibLAS.xcoord(p)
+        y = LibLAS.ycoord(p)
+        z = LibLAS.zcoord(p)
 
-    x_sum += x
-    y_sum += y
-    z_sum += z
+        x_sum += x
+        y_sum += y
+        z_sum += z
+    end
+
+    x_avg = x_sum / n
+    y_avg = y_sum / n
+    z_avg = z_sum / n
+    x_avg, y_avg, z_avg
 end
 
-x_avg = x_sum / n
-y_avg = y_sum / n
-z_avg = z_sum / n
+x_avg, y_avg, z_avg = mean_coordinates(header, reader)
 
 @test x_avg ≈ 1442694.2739025319
 @test y_avg ≈ 377449.24373880465
